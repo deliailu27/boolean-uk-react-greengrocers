219241c24566c5f30a328e6154f97c17048ae11a
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-07-18 14:36:59 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './styles/reset.css'
 import './styles/index.css'
 
 import initialStoreItems from './store-items'
+import { useState } from 'react'
+import StoreItemList from './components/StoreItemList'
+import CartItemList from './components/CartItemList'
 
 /*
 Here's what a store item should look like
@@ -18,20 +21,26 @@ console.log(initialStoreItems)
 
 export default function App() {
   // Setup state here...
+  const [storeItems, setStoreItems] = useState(initialStoreItems)
+  const [cartItems, setCartItems] = useState([])
 
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Wrtite some code here... */}
+          <StoreItemList
+            storeItems={storeItems}
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+          />
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Wrtite some code here... */}
+            <CartItemList cartItems={cartItems} setCartItems={setCartItems} />
           </ul>
         </div>
         <div className="total-section">
--- src/components/StoreItemList.js
+++ src/components/StoreItemList.js
@@ -2,15 +2,21 @@ import { useState } from 'react'
 
 const StoreItemList = props => {
   const storeItems = props.storeItems
+  const cartItems = props.cartItems
 
-  const [ItemsAdded, setItemsAdded] = useState(false)
+  const addItem = item => {
+    const newCart = [...cartItems]
+    newCart.push(item)
+    props.setCartItems(newCart)
+    console.log(item)
+  }
 
   return storeItems.map(item => (
     <li className="storeItem">
       <div class="store--item-icon">
-        <img src="/assets/icons/.svg" alt={item.id} />
+        <img src={`/assets/icons/${item.id}.svg`} alt={item.id} />
       </div>
-      <button onClick={addItem}>Add to cart</button>
+      <button onClick={() => addItem(item)}>Add to cart</button>
     </li>
   ))
 }
